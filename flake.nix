76ed4d043786d0598d2232d501cd76209e473877
Mark Mandriota <myriaxont@gmail.com>
2025-06-30 23:32:40 +0200

    refactor: explicit commonConfig; unifying configuration

--- flake.nix
+++ flake.nix
@@ -20,10 +20,9 @@
   outputs =
     { self, fenix, nix-darwin, nixpkgs, home-manager, nix-homebrew }@inputs:
     let
-      configuration = { pkgs, lib, ... }: {
-        nix.linux-builder.enable = true;
-
+      commonConfig = { pkgs, lib, ... }: {
         nixpkgs.overlays = [ fenix.overlays.default ];
+
         environment.systemPackages = with pkgs; [
           vim
           ((emacsPackagesFor (emacs.override {
@@ -40,10 +39,6 @@
 
           gnupg
 
-          openjdk
-          poetry
-          python3
-          nodejs
           gcc
           go
           (pkgs.fenix.complete.withComponents [
@@ -60,17 +55,159 @@
 
           bear
           tokei
+
           fish
           zoxide
           fzf
 
-          qemu
           libqalculate
+        ];
+
+        nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      };
+
+      darwinConfig = { pkgs, lib, ... }: {
+        nix.linux-builder.enable = true;
+
+        environment.systemPackages = with pkgs; [
+          openjdk
+          poetry
+          python3
+          nodejs
+
+          qemu
           telegram-desktop
           (callPackage ./pkgs/knockknock { })
         ];
 
-        nix.settings.experimental-features = [ "nix-command" "flakes" ];
+        # Homebrew configuration
+        environment.variables.HOMEBREW_NO_ANALYTICS = "1";
+        homebrew = {
+          enable = true;
+          casks = [
+            "playcover-community"
+            "megasync"
+            "lulu"
+            "onlyoffice"
+            "krtirtho/apps/spotube"
+            "krita"
+            "altserver"
+          ];
+          brews = [ "smartmontools" "enchant" ];
+        };
+
+        # Key remapping
+        #
+        # | From      | To           |
+        # |-----------+--------------|
+        # | Caps Lock | Left Control |
+        # | Tab       | Escape       |
+        # | Escape    | Tab          |
+        launchd.user.agents.remap-keys = {
+          serviceConfig = {
+            ProgramArguments = [
+              "/usr/bin/hidutil"
+              "property"
+              "--set"
+              ''
+                {
+                  "UserKeyMapping":[
+                    {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},
+                    {"HIDKeyboardModifierMappingSrc":0x70000002B,"HIDKeyboardModifierMappingDst":0x700000029},
+                    {"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x70000002B}
+                  ]
+                }''
+            ];
+            RunAtLoad = true;
+          };
+        };
+
+        # macOS system defaults
+        system.defaults = {
+          dock = {
+            autohide = true;
+            mru-spaces = false;
+            # Hot corners
+            wvous-bl-corner = 1; # bottom-left corner  : disabled
+            wvous-br-corner = 1; # bottom-right corner : disabled
+            wvous-tl-corner = 1; # top-left corner     : disabled
+            wvous-tr-corner = 1; # top-right corner    : disabled
+          };
+
+          finder = {
+            AppleShowAllExtensions = true;
+            AppleShowAllFiles = true;
+            CreateDesktop = false;
+            FXEnableExtensionChangeWarning = false;
+            FXDefaultSearchScope = "SCcf";
+            FXPreferredViewStyle = "Nlsv";
+            NewWindowTarget = "Other";
+            NewWindowTargetPath = "~/";
+            QuitMenuItem = true;
+            ShowExternalHardDrivesOnDesktop = false;
+            ShowRemovableMediaOnDesktop = false;
+            ShowStatusBar = true;
+            _FXShowPosixPathInTitle = true;
+            _FXSortFoldersFirst = true;
+          };
+
+          hitoolbox.AppleFnUsageType = "Do Nothing";
+
+          loginwindow = {
+            DisableConsoleAccess = true;
+            GuestEnabled = false;
+            SHOWFULLNAME = true;
+          };
+
+          NSGlobalDomain.AppleShowScrollBars = "WhenScrolling";
+
+          menuExtraClock.Show24Hour = true;
+
+          screencapture.location = "~/Pictures/screen";
+
+          trackpad = {
+            ActuationStrength = 0;
+            Clicking = true;
+          };
+        };
+
+        home-manager.users.mark = {
+          home.stateVersion = "25.05";
+          programs = {
+            fish = {
+              enable = true;
+              shellAliases = {
+                th = "trash";
+                dr = "darwin-rebuild switch --flake $HOME/.config/nix";
+              };
+              interactiveShellInit = ''
+                fish_default_key_bindings
+                bind \cr 'history | fzf --tac | read -l result; and commandline -- $result'
+              '';
+            };
+            zoxide = {
+              enable = true;
+              enableFishIntegration = true;
+            };
+            fzf = {
+              enable = true;
+              enableFishIntegration = true;
+            };
+          };
+        };
+
+        programs.fish.enable = true;
+
+        # System metadata
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.stateVersion = 6;
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        # User configuration
+        users.users.mark = {
+          home = "/Users/mark";
+          shell = pkgs.fish;
+        };
       };
     in {
       # $ darwin-rebuild build --flake .#spectral
@@ -78,8 +215,8 @@
         system = "aarch64-darwin";
         specialArgs = inputs;
         modules = [
-          configuration
-          ./hosts/mac.nix
+          commonConfig
+          darwinConfig
           home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
